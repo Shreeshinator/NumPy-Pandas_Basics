126f86629c0b628c26cde557b0a8ffe940f7eaf8
Shreeshinator <shreeshtripathi13@gmail.com>
2026-06-02 10:01:15 +0530

    update slicing

--- 03_slicing.py
+++ 03_slicing.py
@@ -4,3 +4,7 @@ arr = np.array([[1, 2, 3, 4],
                 [5, 6, 7, 8],
                 [9, 10, 11, 12],
                 [13, 14, 15, 16]]) # 4x4 array
+
+# array[start:stop:step] - slicing syntax
+print(arr[0:2,0:2]) # Slicing the top-left 2x2 subarray
+print(arr[1:3,1:3]) # Slicing the middle 2x2 subarray
